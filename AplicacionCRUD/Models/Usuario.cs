b2db820dab8bb7a34d10c04ad6d1a72ebddb58fa
KyronMeza <KyronMeza@DESKTOP-1737GDA>
2021-09-15 18:00:49 -0300

    Agregar archivos de proyecto.

---
+++ AplicacionCRUD/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace AplicacionCRUD.Models
+{
+    public partial class Usuario
+    {
+        public string CodigoUsuario { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+    }
+}
---
+++ AplicacionCRUD/Models/sistemaUsuariosContext.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace AplicacionCRUD.Models
+{
+    public partial class sistemaUsuariosContext : DbContext
+    {
+        public sistemaUsuariosContext()
+        {
+        }
+
+        public sistemaUsuariosContext(DbContextOptions<sistemaUsuariosContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=sistemaUsuarios;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Modern_Spanish_CI_AS");
+
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.HasKey(e => e.CodigoUsuario);
+
+                entity.ToTable("usuarios");
+
+                entity.Property(e => e.CodigoUsuario)
+                    .HasMaxLength(64)
+                    .HasColumnName("codigoUsuario");
+
+                entity.Property(e => e.Apellido)
+                    .IsRequired()
+                    .HasMaxLength(64);
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(64)
+                    .HasColumnName("email");
+
+                entity.Property(e => e.Nombre)
+                    .IsRequired()
+                    .HasMaxLength(64);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(64)
+                    .HasColumnName("password");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
